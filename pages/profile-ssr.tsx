0b7021ae6dc9bdd452b17e5010b9500291635f75
Nafis Fuad <fuadbd14@gmail.com>
2020-01-11 14:03:54 +0600

    Add ssr profile

---
+++ pages/profile-ssr.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import withAuth from "../components/withAuth";
+
+const Profile = ({ user }) => (
+  <Layout user={user}>
+    <h1>Profile</h1>
+
+    <div>
+      <h3>Profile (server rendered)</h3>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  </Layout>
+);
+
+export default withAuth(Profile);
