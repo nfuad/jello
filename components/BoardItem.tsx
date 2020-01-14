291b528bec0dc43ee0f0e737a17e4d54166a42cc
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 00:40:05 +0600

    Add description, update styles

--- components/BoardItem.tsx
+++ components/BoardItem.tsx
@@ -1,35 +1,34 @@
-import * as React from "react";
-import Link from "next/link";
-import randomColor from "randomcolor";
+import * as React from 'react'
+import Link from 'next/link'
 
-export default ({ name, id }) => {
-  const color = randomColor({
-    luminosity: "dark",
-    format: "rgb",
-    alpha: 1
-  });
+export default ({ name, description, id, backgroundColor }) => {
   return (
     <li>
       <Link href={`/boards/${id}`}>
-        <a style={{ background: color }}>{name}</a>
+        <a style={{ backgroundColor }}>
+          <span className="board-name">{name}</span>
+          <hr />
+          <span className="board-description">{description}</span>
+        </a>
       </Link>
       <style jsx>{`
         li {
           list-style-type: none;
           padding: 0;
           margin: 0 2% 8px 0;
-          max-width: 12vw;
+          max-width: 15vw;
         }
 
         a {
           line-height: 20px;
           padding: 10px;
           border: none;
-          height: 8vw;
-          width: 12vw;
+          height: 10vw;
+          width: 15vw;
           border-radius: 5px;
           color: white;
           display: flex;
+          flex-direction: column;
           justify-content: center;
           align-items: center;
           transition: box-shadow 0.2s, opacity 0.25s;
@@ -40,7 +39,19 @@ export default ({ name, id }) => {
           box-shadow: var(--box-shadow);
           opacity: 0.8;
         }
+
+        hr {
+          width: 50%;
+        }
+
+        span.board-name {
+          font-size: 1.2rem;
+        }
+
+        span.board-description {
+          font-size: 0.9rem;
+        }
       `}</style>
     </li>
-  );
-};
+  )
+}
