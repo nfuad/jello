36985bbb600c2e021267cf4aeae6db3441dc09c1
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 00:47:55 +0600

    Update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,15 @@
-import * as React from "react";
+import * as React from 'react'
 
 // custom imports
-import Layout from "../components/Layout";
-import { useFetchUser } from "../utils/user";
+import Layout from '../components/Layout'
 
-export default () => {
-  const { user, loading } = useFetchUser();
-
-  return (
-    <Layout user={user} loading={loading}>
-      <h1>About</h1>
-      <p>
-        This is the about page, navigating between this page and <i>Home</i> is
-        always pretty fast. However, when you navigate to the <i>Profile</i>{" "}
-        page it takes more time because it uses SSR to fetch the user first;
-      </p>
-    </Layout>
-  );
-};
+export default () => (
+  <Layout container={true}>
+    <h1>About</h1>
+    <p>
+      This is the about page, navigating between this page and <i>Home</i> is
+      always pretty fast. However, when you navigate to the <i>Profile</i> page
+      it takes more time because it uses SSR to fetch the user first;
+    </p>
+  </Layout>
+)
