87265a204e167686d9d11bd87f375b1e4e827100
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 00:49:14 +0600

    Update and clean up profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,24 +1,46 @@
-import * as React from "react";
+import * as React from 'react'
 
 // custom imports
-import Layout from "../components/Layout";
-import { useFetchUser } from "../utils/user";
+import Layout from '../components/Layout'
+import Loader from '../components/Loader'
+import { useFetchUser } from '../utils/user'
 
 export default () => {
-  const { user, loading } = useFetchUser();
+  const { user, loading } = useFetchUser()
 
   return (
-    <Layout user={user} loading={loading}>
-      <h1>Profile</h1>
-
-      {loading && <p>Loading profile...</p>}
-
-      {!loading && user && (
+    <Layout user={user} loading={loading} container={true}>
+      {loading && <Loader />}
+      {!loading && !user && <h1>Please log in or create an account.</h1>}
+      {user && (
         <>
-          <p>Profile:</p>
-          <pre>{JSON.stringify(user, null, 2)}</pre>
+          <h1>{user.nickname}'s Profile Info:</h1>
+          <ul>
+            <li>Username: {user.nickname}</li>
+            <li>Email: {user.name}</li>
+            <li>
+              Avatar: <img src={`${user.picture}`} />
+            </li>
+            <li>Updated At: {new Date(user.updated_at).toString()}</li>
+          </ul>
+
+          <style jsx>{`
+            h1 {
+              text-transform: uppercase;
+            }
+
+            li {
+              font-size: 2rem;
+            }
+
+            img {
+              display: block;
+              border-radius: 5px;
+              margin-left: 10%;
+            }
+          `}</style>
         </>
       )}
     </Layout>
-  );
-};
+  )
+}
