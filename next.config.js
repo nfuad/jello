ccd7cd2f885e2d2be18c7ce67a3dcb14e150a910
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 00:37:54 +0600

    Update config format

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
-if (process.env.NODE_ENV !== "production") {
-  const dotenv = require("dotenv");
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv')
 
-  const result = dotenv.config();
+  const result = dotenv.config()
 
   result.error
     ? console.error(result.error)
-    : console.log("The parsed env vars:", result.parsed);
+    : console.log('The parsed env vars:', result.parsed)
 }
 
 module.exports = {
-  target: "serverless",
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
+  target: 'serverless',
   env: {
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
@@ -18,6 +28,8 @@ module.exports = {
     REDIRECT_URI: process.env.REDIRECT_URI,
     POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI,
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
-    SESSION_COOKIE_LIFETIME: process.env.SESSION_COOKIE_LIFETIME
-  }
-};
+    SESSION_COOKIE_LIFETIME: process.env.SESSION_COOKIE_LIFETIME,
+    FAUNADB_SECRET: process.env.FAUNADB_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
+}
