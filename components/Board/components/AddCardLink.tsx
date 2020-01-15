e150357eefb7a08572a3d8f6087cdd9c238bfc2d
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 10:33:48 +0600

    Add card link create

---
+++ components/Board/components/AddCardLink.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+// custom imports
+import { AddCardLink } from '../styles/Base'
+
+export default ({ onClick }) => (
+  <AddCardLink onClick={onClick}>Click to add card</AddCardLink>
+)
