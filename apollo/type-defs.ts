bf4ebc3ad2f5764b6745e1a42400a05ec5c8f644
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 15:27:25 +0600

    Update type defs, add new queries and mutations

--- apollo/type-defs.ts
+++ apollo/type-defs.ts
@@ -1,13 +1,72 @@
 import gql from 'graphql-tag'
 
 export const typeDefs = gql`
-  type User {
+  type Board {
     id: ID!
-    name: String!
-    status: String!
+    title: String!
+    description: String!
+    created_at: String!
+    owner: String!
+    lanes: [Lane]
+    backgroundColor: String!
+  }
+
+  type Lane {
+    id: ID!
+    title: String
+    currentPage: Int
+    cards: [Card]
+  }
+
+  type Card {
+    id: ID!
+    title: String
+    description: String
+    laneId: ID!
+    label: String
+  }
+
+  input LaneInput {
+    id: ID!
+    title: String
+    currentPage: Int
+    cards: [CardInput]
+  }
+
+  input CardInput {
+    id: ID!
+    title: String
+    description: String
+    laneId: String!
+    label: String
+  }
+
+  input CreateBoardInput {
+    title: String
+    description: String
+    owner: String
+    backgroundColor: String
+  }
+
+  input UpdateBoardInput {
+    id: ID
+    lanes: [LaneInput]
+    owner: String
+  }
+
+  input DeleteBoardInput {
+    id: ID
+    owner: String
   }
 
   type Query {
-    viewer: User
+    boards(owner: String): [Board]
+    user(id: ID): User!
+  }
+
+  type Mutation {
+    createBoard(data: CreateBoardInput!): Board!
+    updateBoard(data: UpdateBoardInput!): Board!
+    deleteBoard(data: DeleteBoardInput!): Board!
   }
 `
