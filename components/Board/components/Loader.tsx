9c01544023b4556e67507ba3469d235c1b5bbec8
Nafis Fuad <fuadbd14@gmail.com>
2020-01-15 10:32:26 +0600

    Add loader

---
+++ components/Board/components/Loader.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+// custom imports
+import { LoaderDiv, LoadingBar } from '../styles/Loader'
+
+export default () => (
+  <LoaderDiv>
+    <LoadingBar />
+    <LoadingBar />
+    <LoadingBar />
+    <LoadingBar />
+  </LoaderDiv>
+)
