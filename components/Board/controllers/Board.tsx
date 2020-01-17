44f1dd06890d8d960ae2222f117bd3b975e3de7b
Nafis Fuad <fuadbd14@gmail.com>
2020-01-17 22:41:05 +0600

    Update Board controller

--- components/Board/controllers/Board.tsx
+++ components/Board/controllers/Board.tsx
@@ -4,36 +4,47 @@ import classNames from 'classnames'
 import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import uuidv1 from 'uuid/v1'
-
-// custom imports
 import BoardContainer from './BoardContainer'
 import boardReducer from '../reducers/BoardReducer'
 
 const middlewares = process.env.REDUX_LOGGING ? [logger] : []
 
 interface Props {
-  id: string
-  className: string
+  className: any
   components: any
 }
 
-export default (props: Props) => {
-  const getStore = () => {
+export default class Board extends React.Component<Props, any> {
+  store
+  id
+
+  constructor(props) {
+    super(props)
+    this.store = this.getStore()
+    this.id = props.id || uuidv1()
+  }
+
+  getStore = () => {
     //When you create multiple boards, unique stores are created for isolation
     return createStore(boardReducer, applyMiddleware(...middlewares))
   }
 
-  const store = getStore()
-  const id = props.id || uuidv1()
-  const { className, components } = props
-  const allClassNames = classNames('react-trello-board', className || '')
-
-  return (
-    <Provider store={store}>
-      <>
-        <components.GlobalStyle />
-        <BoardContainer id={id} {...props} className={allClassNames} />
-      </>
-    </Provider>
-  )
+  render() {
+    const { className, components } = this.props
+    const allClassNames = classNames('react-trello-board', className || '')
+    return (
+      // @ts-ignore
+      <Provider store={this.store}>
+        <>
+          <components.GlobalStyle />
+          <BoardContainer
+            // @ts-ignore
+            id={this.id}
+            {...this.props}
+            className={allClassNames}
+          />
+        </>
+      </Provider>
+    )
+  }
 }
