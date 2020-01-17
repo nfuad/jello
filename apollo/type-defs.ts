d4328bcc1fa0559bdc991c38359b67994edb1bed
Nafis Fuad <fuadbd14@gmail.com>
2020-01-17 22:42:36 +0600

    Update type defs

--- apollo/type-defs.ts
+++ apollo/type-defs.ts
@@ -2,71 +2,67 @@ import gql from 'graphql-tag'
 
 export const typeDefs = gql`
   type Board {
-    id: ID!
-    title: String!
-    description: String!
-    created_at: String!
-    owner: String!
+    id: ID
+    title: String
+    description: String
+    created_at: String
+    owner: String
     lanes: [Lane]
-    backgroundColor: String!
+    backgroundColor: String
   }
 
   type Lane {
-    id: ID!
+    id: ID
     title: String
     currentPage: Int
     cards: [Card]
   }
 
   type Card {
-    id: ID!
+    id: ID
     title: String
     description: String
-    laneId: ID!
+    laneId: ID
     label: String
   }
 
   input LaneInput {
-    id: ID!
+    id: ID
     title: String
     currentPage: Int
     cards: [CardInput]
   }
 
   input CardInput {
-    id: ID!
+    id: ID
     title: String
     description: String
-    laneId: String!
+    laneId: String
     label: String
   }
 
   input CreateBoardInput {
     title: String
     description: String
-    owner: String
-    backgroundColor: String
   }
 
   input UpdateBoardInput {
     id: ID
     lanes: [LaneInput]
-    owner: String
   }
 
   input DeleteBoardInput {
     id: ID
-    owner: String
   }
 
   type Query {
-    boards(owner: String): [Board]
-    user(id: ID): User!
+    board(id: ID): Board
+    boards: [Board]
   }
 
   type Mutation {
-    createBoard(data: CreateBoardInput!): Board!
-    updateBoard(data: UpdateBoardInput!): Board!
-    deleteBoard(data: DeleteBoardInput!): Board!
+    createBoard(data: CreateBoardInput): Board
+    updateBoard(data: UpdateBoardInput): Board
+    deleteBoard(data: DeleteBoardInput): Board
   }
 `
