4e591e8a05ba98fc1b0a1cf8dacc404415b4a6f4
Nafis Fuad <fuadbd14@gmail.com>
2020-01-18 00:10:49 +0600

    Remove api functions

--- pages/api/board-data.ts
+++
@@ -1,7 +0,0 @@
-export default function(req, res) {
-  const db = require('../../db.json')
-  const { boards } = db
-  const data = boards.filter(board => board.owner === req.body.owner)
-  console.log(data)
-  return res.json({ data })
-}
