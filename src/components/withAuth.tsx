c57fd65f7d85d01009d985bbf6ccaa765eb6f703
Nafis Fuad <fuadbd14@gmail.com>
2020-01-29 13:48:04 +0600

    Move everything in src dir

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -1,31 +1,31 @@
-import * as React from "react";
+import * as React from 'react'
 
 // custom imports
-import auth0 from "../utils/auth0";
+import auth0 from '../utils/auth0'
 
 export default function withAuth(InnerComponent) {
   return class Authenticated extends React.Component<any, any> {
     static async getInitialProps({ req, res }) {
-      const session = await auth0.getSession(req);
+      const session = await auth0.getSession(req)
       if (!session || !session.user) {
         res.writeHead(302, {
-          Location: "/api/login"
-        });
-        res.end();
-        return;
+          Location: '/api/login',
+        })
+        res.end()
+        return
       }
 
-      return { user: session.user };
+      return { user: session.user }
     }
 
     constructor(props) {
-      super(props);
+      super(props)
     }
 
     render() {
       return (
         <div>{<InnerComponent {...this.props} user={this.props.user} />}</div>
-      );
+      )
     }
-  };
+  }
 }
